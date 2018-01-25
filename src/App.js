04364ac128beeaf9a15796bb4496a1e0c4273774
Babak Javdan <Babakjav.done@gmail.com>
2018-01-24 23:14:05 -0500

    added rest of files to remote

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
+import Album from './components/Album';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+        <nav>
+            <Link to='/'>Landing</Link>
+            <Link to='/library'> Library </Link>
+            <Link to='album'> Album </Link>
+          </nav>
+         <h1> Bloc Jams </h1>
+        </header>
+
+        <main>
+            <Route exact path="/" Component={Landing} />
+            <Route path="/library" Component={Library} />
+            <Route path="/album" Component={Album} />
+        </main>
+
+      </div>
+    );
+  }
+}
+
+export default App;
